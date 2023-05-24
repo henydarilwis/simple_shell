17132643859d66839c86b7202f26985a85a6c088
Henok &  Yewelsew <Henok &  Yewelsew>
2023-05-24 12:22:15 -0700

    hot fix final

---
+++ realloc.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+
+/**
+ **_memset - fills memory with a constant bytes
+ *@s: the pointer to the memory areas
+ *@b: the byte to fill *s with
+ *@n: the amount of bytes to be fill
+ *Return: (s) a pointer to the memory area ss
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - frees a string of stringss
+ * @pp: string of stringss
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memories
+ * @ptr: pointer to previous malloc'ated blocks
+ * @old_size: byte size of previous blocks
+ * @new_size: byte size of new blocks
+ *
+ * Return: pointer to da ol'block nameen.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
